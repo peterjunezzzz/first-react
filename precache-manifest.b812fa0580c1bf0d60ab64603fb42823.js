4ee77d51fb7ce91288f3d73417bfd9130a09de0d
peterjunezzzz <peter12345qwe@gmail.com>
2022-05-29 20:57:18 +0800

    Updates

---
+++ precache-manifest.b812fa0580c1bf0d60ab64603fb42823.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfca08acc799572c6ec1",
+    "url": "/first-react/static/css/main.b308a651.chunk.css"
+  },
+  {
+    "revision": "dfca08acc799572c6ec1",
+    "url": "/first-react/static/js/main.dfca08ac.chunk.js"
+  },
+  {
+    "revision": "3902b1cac89f18fb0fbc",
+    "url": "/first-react/static/js/1.3902b1ca.chunk.js"
+  },
+  {
+    "revision": "c43d182103702284a2a5",
+    "url": "/first-react/static/js/runtime~main.c43d1821.js"
+  },
+  {
+    "revision": "eac552f32d6797b0e18f7d5819340603",
+    "url": "/first-react/static/media/pin.eac552f3.png"
+  },
+  {
+    "revision": "c7fd5c483bc4ec3412b959510bf3a5fb",
+    "url": "/first-react/static/media/placeholder.c7fd5c48.png"
+  },
+  {
+    "revision": "ab8378ba1dc220e174f1fb0eac563fde",
+    "url": "/first-react/static/media/chronometer.ab8378ba.png"
+  },
+  {
+    "revision": "f1809efd44774103bd301588bfeda9a6",
+    "url": "/first-react/static/media/call.f1809efd.png"
+  },
+  {
+    "revision": "08657617a929dc4a5b99aa9819c2b5c2",
+    "url": "/first-react/static/media/taiwan.08657617.jpg"
+  },
+  {
+    "revision": "9ee88be1efb67d842cab0382d5ce07e9",
+    "url": "/first-react/static/media/spotImg404.9aae9bf1.9ee88be1.png"
+  },
+  {
+    "revision": "c7ee328293e0ee70670f5fee6faeb3bc",
+    "url": "/first-react/static/media/dollar.c7ee3282.png"
+  },
+  {
+    "revision": "d68bebf4c0197edad6e132d5e0c4135c",
+    "url": "/first-react/static/media/fast-forward.d68bebf4.png"
+  },
+  {
+    "revision": "8dc197c37adfd2d62569c9b2bb2340d1",
+    "url": "/first-react/static/media/activity404.19912b87.8dc197c3.png"
+  },
+  {
+    "revision": "892b031289da1a8d05587818d96b7541",
+    "url": "/first-react/static/media/restaurant.892b0312.png"
+  },
+  {
+    "revision": "c76e9473c92674a05246767c2e38d348",
+    "url": "/first-react/index.html"
+  }
+];
